77d75bd3ee53b980f50350ae4bfe54912f1f5dfd
Andy Holmes <andyavd01@gmail.com>
2018-11-10 07:07:47 -0500

    add flying b-ball

---
+++ orbit/js/main.js
@@ -0,0 +1,13 @@
+var basketball = document.querySelector("img");
+var angle = 0, lastTime = null;
+
+function animate(time) {
+  if(lastTime != null) {
+    angle += (time - lastTime) * 0.001;
+  }
+  lastTime = time;
+  basketball.style.top = (Math.sin(angle) * 20) + "px";
+  basketball.style.left = (Math.cos(angle) * 200) + "px";
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
